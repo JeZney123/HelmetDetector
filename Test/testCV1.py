bf4794f1038d5037f79a056a2bf727c7fb2a65cf
NarueponCy <narueponeischaiya@gmail.com>
2018-06-09 04:36:00 +0700

    Test1

---
+++ Test/testCV1.py
@@ -0,0 +1,19 @@
+import numpy as np
+import cv2
+
+picamera = cv2.VideoCapture(0)
+webcam = cv2.VideoCapture(1)
+
+while(True):
+    ret1, frame1 = picamera.read()
+    ret2, frame2 = webcam.read()
+    gray = cv2.cvtColor(frame1, cv2.COLOR_BGR2GRAY)
+ 
+    cv2.imshow('Picamera',gray)
+    cv2.imshow('Webcam',frame2)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+    
+picamera.release()
+webcam.release()
+cv2.destroyAllWindows()
